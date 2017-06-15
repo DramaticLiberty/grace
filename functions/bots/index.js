20497006c783dd49388ac0ca322d83ec52df5d45
Mihai Balint <balint.mihai@gmail.com>
2017-06-15 11:10:46 +0300

    Update index

--- functions/bots/index.js
+++ functions/bots/index.js
@@ -2,7 +2,68 @@ console.log('starting function')
 const AWS = require('aws-sdk');
 const lexmodelbuildingservice = new AWS.LexModelBuildingService({apiVersion: '2017-04-19'});
 
+const projectName = {
+    name: 'ProjectName',
+    description: 'The names of all our projects'
+    enumerationValues: [
+        {value: 'Entry'},
+        {value: 'eSource'},
+        {value: 'Conduct'},
+        {value: 'Loader'},
+        {value: 'Review'},
+        {value: 'Coding'},
+        {value: 'Manager'}
+    ]
+};
 
+const intent = {
+    name: 'CodeRepositories',
+    sampleUtterances: [
+        "Where is the source code?",
+        "What is the code repository for {Project}?",
+        "Where is the code repository for {Project}?",
+        "Where is the code for {Project}?",
+        "What is the repository for {Project} source code?"
+    ],
+    slots: [{
+        name: "Project",
+        description: "The name of a project.",
+        priority: 3,
+        valueElicitationPrompt: {
+            maxAttempts: 1,
+            messages: [
+                {contentType: "PlainText", content: "For what project?"},
+                {contentType: "PlainText", content: "What project?"}
+            ]
+        },
+        sampleUtterances: [
+            "Project {Project}.",
+            "App {Project}.",
+            "Webapp {Project}."
+            "{Project}."
+        ],
+        slotConstraint: "Required",
+        slotType: "ProjectName",
+        slotTypeVersion: "$LATEST"
+    }],
+    confirmationPrompt: {
+        maxAttempts: 0,
+        messages: []
+    },
+    rejectionStatement: {
+        messages: []
+    },
+    conclusionStatement: {
+        messages: [
+            {contentType: "PlainText", content: "Anything else?"},
+            {contentType: "PlainText", content: "Can I help with anything else?"}
+        ],
+        responseCard: "foo"
+    },
+    fulfillmentActivity: {
+        type: "ReturnIntent"
+    }
+};
 
 exports.handle = function(e, ctx, cb) {
     console.log('processing event: %j', e)
@@ -18,4 +79,4 @@ exports.handle = function(e, ctx, cb) {
         }
         cb(null, { the_bots: data.bots });
     });
-}
+};
